08464b809a3ded719abecfac684efaec7f2e678b
semihtr13 <semihtr13@gmail.com>
2020-04-19 03:37:45 +0300

    splash activity

--- app/src/main/java/com/setsoft/math/SplashActivity.java
+++ app/src/main/java/com/setsoft/math/SplashActivity.java
@@ -2,17 +2,13 @@ package com.setsoft.math;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.res.Resources;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        int a=R.string.app_name;
-
-
+        setContentView(R.layout.activity_splash);
     }
 }
